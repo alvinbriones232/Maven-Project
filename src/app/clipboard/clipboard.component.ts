c3c2d0c337a92654ee3285f3bd4b7199daec594a
Alvin <abriones@vjsol.jp>
2018-09-15 21:42:52 +0800

    initial commit

---
+++ src/app/clipboard/clipboard.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-clipboard',
+  templateUrl: './clipboard.component.html',
+  styleUrls: ['./clipboard.component.css']
+})
+export class ClipboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
